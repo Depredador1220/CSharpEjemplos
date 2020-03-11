667966ff9219d10d10254c798c9a93b00202b269
Christian Kenny <christiankennyruizvalenzuela@gmail.com>
2020-03-10 21:35:22 -0600

    Primer ejemplo

---
+++ Csharp/Csharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Mi primera app";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*************************************");
+            Console.WriteLine("***** Bienvenido a mi app *****");
+            Console.WriteLine("*************************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+            
+            Console.ReadLine();
+        }
+    }
+}
