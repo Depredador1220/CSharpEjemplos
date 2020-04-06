f2b97902d4f7586540dfedf64a0c5438c2ff0de2
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-04-06 15:57:51 -0500

    Create FuncionalidadObject.cs

---
+++ FuncionalidadObject.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            FuncionalidadObjeto();
+            Console.ReadLine();
+        }
+
+        static void FuncionalidadObjeto()
+        {
+            Console.WriteLine("Funcionalidad de System.Object");
+
+            Console.WriteLine("12.GetHashCode() = {0}", 12.GetHashCode());
+            Console.WriteLine("12.Equals(23) = {0}", 12.Equals(23));
+            Console.WriteLine("12.ToString() = {0}", 12.ToString());
+            Console.WriteLine("12.GetType() = {0}", 12.GetType());
+            Console.WriteLine();
+        }
+    }
+}
